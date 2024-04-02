31bd325e75c391ae2ed725d3c0cfd17543369f4c
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2024-04-02 17:38:33 +0500

    leadership page in progress

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Community from './Pages/Home/Community';
 import Ceo from './Pages/Home/Ceo';
+import Leadership from './Pages/Home/Leadership';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path='/about/career' element={<Career />} />
           <Route path='/about/community' element={<Community/>} />
           <Route path='/about/ceo' element={<Ceo/>}/>
+          <Route path='/about/leadership' element={<Leadership/>}/>
         </Routes >
       </BrowserRouter>
     </>
---
+++ src/Pages/Home/Leadership.jsx
@@ -0,0 +1,10 @@
+import Header from "../../Components/Header/Header"
+import TextImageGroup1 from "../../Components/Leadership/TextImageGroup1"
+
+const Leadership=()=>{
+    return(<>
+    <Header/>
+    <TextImageGroup1/>
+    </>)
+}
+export default Leadership
