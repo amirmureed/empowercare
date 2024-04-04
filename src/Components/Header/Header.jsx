0432f763b2154a13721bef2e803a53c616a9db74
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2024-04-05 00:19:59 +0500

    resource dropdown added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,6 +21,7 @@ const Header = () => {
     const OnMouseEnter=()=> {setOpenModal(true)}
     const OnMouseLeave=()=> {setOpenModal(false)}
     const [open,setOpen]=useState(false)
+    const [openResources,setOpenResources]=useState(false)
     const theme=useTheme()
     const isMobile = useMediaQuery(theme.breakpoints.down('md'));
     const SmallView=useMediaQuery(theme.breakpoints.down('xs'))
@@ -37,29 +38,63 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     
                     <Navbar.Collapse id="basic-navbar-nav">
-                        {!open ? ( <Nav className="mr-auto">
+                        {!open && !openResources ? ( <Nav className="mr-auto">
                          <Link to="/" className='nav-link'>
                                 Professionals
                             </Link>
                             <Link to="/facilities" className='nav-link'>
                                 Facilities
                             </Link>
+                            
                             <Link to="/about" className='nav-link' >
                                 About Us
-                                {isMobile ?(<> <IconButton onClick={()=>setOpen(true)} >
-                        <IoMdArrowDropright color='black' />
-                    </IconButton></>):(<></>)}
+                              
                     <div className='dropdown'>
                         <span className='d-span'></span><br />
                         <div className='dropdown-content'>
-                <PopoverContent/>
+                <PopoverContent
+                 route1={'/about/ceo'} route2={'/about/community'} route3={'/about/career'}
+                 name1={'Ceo'} name2={'Community'} name3={'Career'}
+                />
                 </div>
             </div>
-                            </Link>
+            {isMobile ?(<> <IconButton onClick={()=>setOpen(true)} >
+                        <IoMdArrowDropright color='black' />
+                    </IconButton></>):(<></>)} 
+                            </Link>  
+                    
+                        <Link to="/resources/referral-program" className='nav-link'>
+                            Resources
                            
-                        </Nav>):(<>
+                    <div className='dropdown'>
+                        <span className='d-span'></span><br />
+                        <div className='dropdown-content'>
+                <PopoverContent
+                  route1={'/resources/referral-program'} route2={'/resources/network'} route3={'/resources/shift-makers'}
+                  name1={'Referral Program'} name2={'Empower your Network'} name3={'Shift Makers'}
+                />
+                </div>
+            </div>
+            
+            {isMobile ?(<> <IconButton  onClick={()=>setOpenResources(true)} >
+                        <IoMdArrowDropright color='black' />
+                    </IconButton></>):(<></>)} 
+                        </Link>
+                           
+                        </Nav>): open?(<>
                             <IconButton onClick={()=>setOpen(false)}><FaArrowCircleLeft></FaArrowCircleLeft> </IconButton>
-                        <PopoverContent></PopoverContent></>)}
+                        <PopoverContent
+                         route1={'/about/ceo'} route2={'/about/community'} route3={'/about/career'}
+                         name1={'Ceo'} name2={'Community'} name3={'Career'}
+                        /></>)
+                    :openResources?(<>
+                     <IconButton onClick={()=>setOpenResources(false)}><FaArrowCircleLeft></FaArrowCircleLeft> </IconButton>
+                        <PopoverContent
+                         route1={'/resources/referral-program'} route2={'/resources/network'} route3={'/resources/shift-makers'}
+                         name1={'Referral Program'} name2={'Empower your Network'} name3={'Shift Makers'}
+                        /></>
+                    ):(<></>)
+                    }
                        
                         <Form inline>
                             <Row>
