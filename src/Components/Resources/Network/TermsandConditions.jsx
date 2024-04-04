105b47d941403f306cc5bca382eb3420ca8a5e42
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2024-04-04 21:34:37 +0500

    network page completed

--- src/Components/Resources/Network/TermsandConditions.jsx
+++ src/Components/Resources/Network/TermsandConditions.jsx
@@ -1,13 +1,16 @@
+import './TermsandConditions.scss'
 const TermsandConditions=()=>{
     return(<>
     <div className="terms-conditions">
-        
+        <div className="tc-sec-bg">
                 <div className="container">
                     <div className="tc-wrapper">
                         <div className="top-content">
+                            
                             <h6 style={{color: '#4EB3E8'}}>TERMS & CONDITIONS</h6>
                             <h2 className="heading">REFERRAL PROGRAM <span className="text_grad_red"><br />TERMS & CONDITIONS</span></h2><br />
-                            <div className="t&c-list">
+                            
+                           
                                 <ul>
                                     <li>Referrals must be submitted through our online form, email, or directly to an EmpowerCare recruiter. The name and contact information for both the referrer and the referral are required.</li><br />
                                     <li>To be eligible for the referral bonus, the referred candidate must be a new contact for EmpowerCare (not already in EmpowerCare's database or have worked with the company within the past 12 months).</li><br />
@@ -16,12 +19,12 @@ const TermsandConditions=()=>{
                                     <li>If you qualify for a referral bonus, you will receive your bonus automatically through your preferred payment method. Bonuses are processed and paid out on the next pay cycle after the referred healthcare professional completes 120 billed hours.</li><br />
                                     <li>All taxes, including federal, state, and local, are the sole responsibility of the referrer, in accordance with all applicable laws and regulations. The referrer will be required to provide their social security number for tax reporting purposes. The value of the bonus will be reported on the applicable IRS form (W-2 or 1099)</li>
                                 </ul>
-                            </div>
+                            
                             
                         </div>
                     </div>
                 </div>
-        
+                </div>
         </div>
     </>)
 }
