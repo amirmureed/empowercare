bcadc47ff933062cff467f7da53b71098e7e6207
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2024-04-08 04:43:31 +0500

    staffing page in progress

---
+++ src/Pages/Home/Non-Clinical.jsx
@@ -0,0 +1,27 @@
+import Approach from "../../Components/Approach/Approach"
+import Banner1 from "../../Components/Banner1/Banner1"
+import Banner5 from "../../Components/Banner5/Banner5"
+import Guaranty from "../../Components/Facilities/TruePartner/Guaranty"
+import TrustedPartner from "../../Components/Facilities/TruePartner/TrustedPartner"
+import FaqAccordian from "../../Components/FaqAccordian/FaqAccordian"
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+import Toptalent from "../../Components/Toptalent/Toptalent"
+import Trusted from "../../Components/Trusted/Trusted"
+import Userslider from "../../Components/Userslider/Userslider"
+
+const NonClinical=()=>{
+return(<>
+<Header/>
+<Banner5/>
+<TrustedPartner/>
+<Guaranty/>
+<Approach/>
+<Toptalent/>
+<Trusted/>
+<FaqAccordian/>
+<Userslider/>
+<Footer/>
+</>)
+}
+export default NonClinical
