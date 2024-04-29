5545f4519c66c74eb7c0cb83c78897e05a83cd57
Muhammad Sufyan <sufi.sufyan121@gmail.com>
2024-04-29 18:42:49 +0500

    mobile view menu changings

--- src/Components/HeaderWhite/HeaderWhite.jsx
+++ src/Components/HeaderWhite/HeaderWhite.jsx
@@ -15,6 +15,29 @@ import { IoMdArrowDropright } from "react-icons/io";
 import PopoverContent from "../Common/PopoverContent";
 import Dropdown from "../Dropdown/Dropdown";
 import $ from "jquery";
+import logo1 from "../../Assets/Group 607.png";
+import logo2 from "../../Assets/Group 608.png";
+import logo3 from "../../Assets/Group 609.png";
+import logo4 from "../../Assets/Group 610.png";
+import logo5 from "../../Assets/Group 611.png";
+import logo6 from "../../Assets/Group 612.png";
+import logo7 from "../../Assets/Group 613.png";
+import logo8 from "../../Assets/Group 614.png";
+import logo9 from "../../Assets/Group 615.png";
+import logo10 from "../../Assets/Group 616.png";
+import logo11 from "../../Assets/Group 620.png";
+import logo12 from "../../Assets/Group 618.png";
+import logo13 from "../../Assets/Group 619.png";
+import logo14 from "../../Assets/Group 626.png";
+import logo15 from "../../Assets/Group 627.png";
+import logo16 from "../../Assets/Group 628.png";
+import logo17 from "../../Assets/Group 629.png";
+import logo18 from "../../Assets/Group 630.png";
+import logo19 from "../../Assets/Group 631.png";
+import logo20 from "../../Assets/Group 632.png";
+import logo21 from "../../Assets/Group 633.png";
+import logo22 from "../../Assets/Group 634.png";
+import IconsWithContent from "../IconsWithContent/IconsWithContent";
 
 const HeaderWhite = ({ logoImg }) => {
   const [open, setOpen] = useState(false);
@@ -26,7 +49,7 @@ const HeaderWhite = ({ logoImg }) => {
   const location = useLocation();
   useEffect(() => {
     const { pathname } = location;
-    
+   $('.nav-link').show()
     if (!isMobile) {
       if (
         pathname === "/professionals" ||
@@ -37,8 +60,8 @@ const HeaderWhite = ({ logoImg }) => {
         pathname === "/professionals/directcare-career" ||
         pathname === "/professionals/guide"
       ) {
-        $('.dropdown-facilities').hide();
-        $('.dropdown').hide();
+        $(".dropdown-facilities").hide();
+        $(".dropdown").hide();
       } else if (
         pathname === "/about" ||
         pathname === "/about/ceo" ||
@@ -46,19 +69,19 @@ const HeaderWhite = ({ logoImg }) => {
         pathname === "/about/community" ||
         pathname === "/about/leadership"
       ) {
-        $('.dropdown-facilities').hide();
-        $('.dropdown-professionals').hide();
+        $(".dropdown-facilities").hide();
+        $(".dropdown-professionals").hide();
       } else if (
         pathname === "/facilities" ||
         pathname === "/facilities/directcare-careers" ||
         pathname === "/facilities/seamless" ||
         pathname === "/facilities/staffing-solutions"
       ) {
-        $('.dropdown-professionals').hide();
-        $('.dropdown').hide();
-      } else if( pathname=== "/"){
-        $('.dropdown-facilities').hide();
-        $('.dropdown').hide();
+        $(".dropdown-professionals").hide();
+        $(".dropdown").hide();
+      } else if (pathname === "/") {
+        $(".dropdown-facilities").hide();
+        $(".dropdown").hide();
       }
     }
   }, [location, isMobile]);
@@ -206,6 +229,7 @@ const HeaderWhite = ({ logoImg }) => {
                 <IconButton onClick={() => setOpenResources(false)}>
                   <FaArrowCircleLeft></FaArrowCircleLeft>{" "}
                 </IconButton>
+
                 <PopoverContent
                   route1={"/resources/referral-program"}
                   route2={"/resources/network"}
@@ -224,6 +248,48 @@ const HeaderWhite = ({ logoImg }) => {
                 <IconButton onClick={() => setOpenFacilities(false)}>
                   <FaArrowCircleLeft></FaArrowCircleLeft>{" "}
                 </IconButton>
+                <IconsWithContent
+                  icon1={logo1}
+                  icon2={logo2}
+                  icon3={logo3}
+                  icon4={logo4}
+                  icon5={logo5}
+                  icon6={logo6}
+                  icon7={logo7}
+                  icon8={logo8}
+                  icon9={logo9}
+                  icon10={logo10}
+                  icon11={logo11}
+                  icon12={logo12}
+                  icon13={logo13}
+                  text1="Non-Clinical Healthcare Staffing by EmpowerCare"
+                  text2="Empowering your Community Health Center with Top Talent"
+                  text3="Empowering Hospitals and Health Systems with Top Talent"
+                  text4="EmpowerCare Corporate Staffing: Empower Employee Health"
+                  text5="Elevating Surgical Excellence with EmpowerCare Staff"
+                  text6="Elevate Student Health: EmpowerCare Education Staffing"
+                  text7="Elevate Student Health with our School Healthcare Staffing"
+                  text8="Elevate Mental Health Care with EmpowerCare Staffing"
+                  text9="Elevate Direct Care with EmpowerCare  Professionals"
+                  text10="Elevate Care: EmpowerCare Advanced Practice Staff"
+                  text11="Elevate Care with EmpowerCare Allied Health Staffing"
+                  text12="Elevate Care Quality: EmpowerCare Skilled Nursing Staffing"
+                  text13="Bridge Leadership Gaps with EmpowerCare Interim Solutions"
+                  route1="/facilities/staffing-solutions"
+                  route2="/facilities/Facilitiesheallthcenter"
+                  route3="/facilities/Facilitieshealthsystem"
+                  route4="/facilities/Facilitiescorporatestaffing"
+                  route5="/facilities/Facilitiessurgicalexellence"
+                  route6="/facilities/Facilitiesstudenthealth"
+                  route7="/facilities/Facilitiesschoolhealth"
+                  route8="/facilities/Facilitiesmentalhealth"
+                  route9="/facilities/Facilitiesellevatedirectcare"
+                  route10="/facilities/Facilitiespracticestaff"
+                  route11="/facilities/Facilitiesalliedhealth"
+                  route12="/facilities/Facilitiesskillednursing"
+                  route13="/facilitiesbridgeleadership"
+                />
+                <hr />
                 <PopoverContent
                   route1={"/facilities/seamless"}
                   name1={"Careers"}
@@ -284,6 +350,35 @@ const HeaderWhite = ({ logoImg }) => {
                 <IconButton onClick={() => setOpenProfessionals(false)}>
                   <FaArrowCircleLeft></FaArrowCircleLeft>{" "}
                 </IconButton>
+                <IconsWithContent
+                  icon1={logo14}
+                  icon2={logo15}
+                  icon3={logo16}
+                  icon4={logo17}
+                  icon5={logo18}
+                  icon6={logo19}
+                  icon7={logo20}
+                  icon8={logo21}
+                  icon9={logo22}
+                  text1="Your Pathway to Meaningful Direct Care Careers"
+                  text2="Your Passport to Unforgettable Travel Nursing Adventures"
+                  text3="Your Gateway to Extraordinary Nursing Opportunities"
+                  text4="Your Gateway to Exciting Allied Health Careers"
+                  text5="Your CNA Career Launchpad"
+                  text6="Non-Clinical Healthcare Careers"
+                  text7="Ignite Your Passion for Mental Health"
+                  text8="Empower Your Nursing Specialty: Find Your Perfect Job Now"
+                  text9="Elevate Your Advanced Practice Nursing Career"
+                  route1="/professional/Professionals2passport"
+                  route2="/professional/Professionals2gatewaynursing"
+                  route3="/professional/Professionals2gatewayexciting"
+                  route4="/professional/Professionals2cnacarrers"
+                  route5="/professional/Professionals2gatewaymeaningful"
+                  route6="/professional/Professionals2ignitepassion"
+                  route7="/professional/Professionals2staffingoppertunities"
+                  route8="/professional/Professionals2elevateadvance"
+                />
+                <hr />
                 <PopoverContent
                   route1={"/professionals/seamless"}
                   name1={"Seamless"}
